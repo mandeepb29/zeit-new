f41312f01829bb525e0a15b37017acdbca112b89
mandeepb29 <mandeepb29@gmail.com>
2020-05-27 04:03:31 +0530

    website all sections styling done

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@
 //CURSOR ANIMATION
 const elements = {
   scrollContainer: document.querySelector(".scroll-container"),
-
+  _3dHoverContainer: document.querySelector("._3d-hover__container"),
+  _3dHoverItem: document.querySelector("._3d-hover__item"),
+  //hoverContainer: document.querySelectorAll(".hover-this"),
+  hoverItem: document.querySelectorAll(".hover-item"),
   cursor: document.querySelector(".cursor"),
   header: document.querySelector("header"),
   navbar: document.querySelector(".navbar"),
@@ -25,6 +28,9 @@ cursorLinksTinyArr = Array.from(elements.cursorLinksTiny);
 techIconsArr = Array.from(elements.techIcons);
 menuLinksArr = Array.from(elements.menuLinks);
 introTextArr = Array.from(elements.introText);
+hoverItemArr = Array.from(elements.hoverItem);
+//_3dHoverContainerArr = Array.from(elements._3dHoverContainer);
+//_3dHoverItemArr = Array.from(elements._3dHoverItem);
 
 document.addEventListener("mousemove", moveCursor);
 
@@ -55,7 +61,6 @@ cursorLinksTinyArr.forEach((e) => {
 cursorLinksTinyArr.forEach((e) => {
   e.addEventListener("mouseleave", removeTinyCursor);
 });
-
 
 introTextArr.forEach((e) => {
   e.addEventListener("mouseover", addBorderCursor);
@@ -106,24 +111,97 @@ elements.header.addEventListener("scroll", ()=> {
   console.log("header scrolled");
 } )
 
+//functions to manipulate hover element
+function hoverEffect(e){
+    const { offsetX: x, offsetY: y } = e,
+    
+    { offsetWidth: width, offsetHeight: height } = this,
+ 
+    move = 25,
+    xMove = x / width * (move * 1.5) - move,
+    yMove = y / height * (move * 1.5) - move;
+
+    //console.log(x,y);
+    this.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+    if (e.type === 'mouseleave') this.style.transform = '';
+}
+
+hoverItemArr.forEach(b => b.addEventListener('mousemove', hoverEffect));
+hoverItemArr.forEach(b => b.addEventListener('mouseleave', hoverEffect));
+
+//------------------------------------------------------------------------------//
+
+//functions to manipulate the 3d hover elements // that are team members image
+
+var onMouseEnterHandler = function(event){
+  update(event);
+}
+
+var onMouseLeaveHandler = function() {
+    elements._3dHoverItem.style = "";
+};
+
+var onMouseMoveHandler = function(event) {
+  if (isTimeToUpdate()) {
+    update(event);
+  }
+};
+
+elements._3dHoverContainer.addEventListener("mouseenter",onMouseEnterHandler);
+
+elements._3dHoverContainer.addEventListener("mouseleave",onMouseLeaveHandler);
+
+elements._3dHoverContainer.addEventListener("mousemove",onMouseMoveHandler);
+
+var counter = 0;
+var updateRate = 10;
+var isTimeToUpdate = function() {
+  return counter++ % updateRate === 0;
+};
+
+var mouse = {
+  _x: 0,
+  _y: 0,
+  x: 0,
+  y: 0,
+  updatePosition: function(event) {
+    var e = event || window.event;
+    this.x = e.clientX - this._x;
+    this.y = (e.clientY - this._y) * -1;
+  },
+  setOrigin: function(e) {
+    this._x = e.offsetLeft + Math.floor(e.offsetWidth/2);
+    this._y = e.offsetTop + Math.floor(e.offsetHeight/2);
+    console.log(e,this.show());
+  },
+  show: function() { return '(' + this.x + ', ' + this.y + ')'; }
+}
+
+// Track the mouse position relative to the center of the container.
+  mouse.setOrigin(elements._3dHoverContainer);
 
 
-//smooth scrolling on webpage
+var update = function(event) {
+  mouse.updatePosition(event);
+  updateTransformStyle(
+    (mouse.y / elements._3dHoverItem.offsetHeight/2).toFixed(2),
+    (mouse.x / elements._3dHoverItem.offsetWidth/2).toFixed(2)
+  );
+};
 
-// $("html").easeScroll({
-//   frameRate: 10,
-//   animationTime: 1200,
-//   stepSize: 40,
-//   pulseAlgorithm: 1,
-//   pulseScale: 8,
-//   pulseNormalize: 1,
-//   accelerationDelta: 20,
-//   accelerationMax: 10,
-//   keyboardSupport: true,
-//   arrowScroll: 50,
-//   touchpadSupport: true,
-//   fixedBackground: true
-// });
+var updateTransformStyle = function(x, y) {
+  var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+  elements._3dHoverItem.style.transform = style;
+  elements._3dHoverItem.style.webkitTransform = style;
+  elements._3dHoverItem.style.mozTransform = style;
+  elements._3dHoverItem.style.msTransform = style;
+  elements._3dHoverItem.style.oTransform = style;
+};
+
+
+//-------------------------------------------------------------------//
+
 
 //naviagtion timeLine
 const navTimeline = gsap.timeline({ paused: true });
@@ -177,6 +255,8 @@ const toggleMenu = () => {
     navTimeline.reverse();
   }
 };
+
+
 
 elements.toggleMenuBtn.addEventListener("click", toggleMenu);
 menuLinksArr.forEach((e) => {
