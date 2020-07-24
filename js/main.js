f777db36a90db3c672b37821debccad4affb81bd
mandeepb29 <mandeepb29@gmail.com>
2020-07-24 14:06:54 +0530

    website content updated

--- js/main.js
+++ js/main.js
@@ -157,21 +157,22 @@ hoverItemArr.forEach((b) =>
 //------------------------------------------------------------------------------//
 
 //functions to manipulate the 3d hover elements // --that are team members image
-_3dHoverContainerArr.forEach((el) =>
-  el.addEventListener("mouseenter", onMouseEnterHandler2)
-);
-_3dHoverContainerArr.forEach((el) =>
-  el.addEventListener("mouseleave", onMouseLeaveHandler2)
-);
-_3dHoverContainerArr.forEach((el) =>
-  el.addEventListener("mousemove", onMouseMoveHandler2)
-);
+
+// _3dHoverContainerArr.forEach((el) =>
+//   el.addEventListener("mouseenter", onMouseEnterHandler2)
+// );
+// _3dHoverContainerArr.forEach((el) =>
+//   el.addEventListener("mouseleave", onMouseLeaveHandler2)
+// );
+// _3dHoverContainerArr.forEach((el) =>
+//   el.addEventListener("mousemove", onMouseMoveHandler2)
+// );
 
 var onMouseEnterHandler2 = function (event) {
   // console.log("mouse enter",this);
   var innerItem = this.querySelector("._3d-hover__item");
-  //mouse.setOrigin(this);
-  update(event, innerItem);
+  mouse.setOrigin(this);
+  //update(event, innerItem);
 };
 
 var onMouseLeaveHandler2 = function () {
@@ -183,20 +184,14 @@ var onMouseLeaveHandler2 = function () {
 var onMouseMoveHandler2 = function (event, el) {
   var innerItem = this.querySelector("._3d-hover__item");
   //console.log("mouse move",this);
+  //console.log(counter);
   if (isTimeToUpdate()) {
     update(event, innerItem);
+   displayMousePositionHelper(event,innerItem,this);
+    //console.log("update!!");
   }
 };
 
-_3dHoverContainerArr.forEach((el) =>
-  el.addEventListener("mouseenter", onMouseEnterHandler2)
-);
-_3dHoverContainerArr.forEach((el) =>
-  el.addEventListener("mouseleave", onMouseLeaveHandler2)
-);
-_3dHoverContainerArr.forEach((el) =>
-  el.addEventListener("mousemove", onMouseMoveHandler2)
-);
 
 var counter = 0;
 var updateRate = 10;
@@ -211,9 +206,10 @@ $(".anchor-link").click(function () {
   el.moveTo(page_index);
 });
 
-// _3dHoverContainerArr.forEach(el => {
-//   console.log($(el).offset().top, $(el).offset().left);
-// })
+_3dHoverContainerArr.forEach(el => {
+  console.log("The offsets - ");
+  console.log($(el).offset().top, $(el).offset().left);
+})
 
 var mouse = {
   _x: 0,
@@ -222,34 +218,50 @@ var mouse = {
   y: 0,
 
   setOrigin: function (itemContainer) {
-    //console.log(itemContainer);
+    console.log(itemContainer);
     this.x = 0;
     this.y = 0;
     this._x =
       itemContainer.offsetLeft + Math.floor(itemContainer.offsetWidth / 2);
     this._y =
       itemContainer.offsetTop + Math.floor(itemContainer.offsetHeight / 2);
+      
     console.log(`x =  ${this.x},y = ${this.y},_x = ${this._x},_y = ${this._y}`);
   },
 
   updatePosition: function (event) {
-    //console.log("Update Position", event);
+    console.log("Update Position", event);
     var e = event;
     this.x = e.clientX - this._x;
     this.y = (e.clientY - this._y) * -1;
-    //console.log(this.show());
   },
+
+  show: function() {
+    return "(" + this.x + ", " + this.y + ")";
+  }
+
 };
 // Track the mouse position relative to the center of the container.
 
 var update = function (event, innerItem) {
-  //console.log(event);
+  console.log(event);
   mouse.updatePosition(event);
+  
   updateTransformStyle(
     (mouse.y / innerItem.offsetHeight / 2).toFixed(2),
     (mouse.x / innerItem.offsetWidth / 2).toFixed(2),
     innerItem
   );
+};
+
+var displayMousePositionHelper = function(event,innerItem,container) {
+  
+  var helper = innerItem.querySelector(".helper");
+  console.log(helper,innerItem,container);
+  var e = event || window.event;
+  helper.innerHTML = mouse.show();
+  helper.style.top = (e.clientY-container.offsetTop)+"px;";
+  helper.style.left = (e.clientX-container.offsetLeft)+"px;";
 };
 
 var updateTransformStyle = function (x, y, item) {
